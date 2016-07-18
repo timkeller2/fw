12b6649d0bb8b067e37238bdf4cfc8bc7315d65f
timkeller2 <tim@kellpro.com>
2016-07-18 17:17:01 -0500

    Animals can have young.  Roughly 1/3% chance per level per turn after level 2.

--- python/CustomFunctions.py
+++ python/CustomFunctions.py
@@ -3097,10 +3097,13 @@ class CustomFunctions:
 					pUnit.setHasPromotion(gc.getInfoTypeForString('PROMOTION_FATIGUED'), True)
 					oNewUnit.convert(pUnit)
 
-				## Giant Spiders can have young
-				if (bCanCreateUnit and CyGame().getSorenRandNum(300, "NewSpider") < pUnit.getLevel() and not pUnit.isHasPromotion(gc.getInfoTypeForString('PROMOTION_FATIGUED')) and (pUnit.getUnitType() == gc.getInfoTypeForString('UNIT_GIANT_SPIDER'))):
-					oNewUnit = gc.getInfoTypeForString('UNIT_BABY_SPIDER')
+				## Animals can have young
+				if (bCanCreateUnit and CyGame().getSorenRandNum(300, "NewAnimal") < pUnit.getLevel() - 1 and not pUnit.isHasPromotion(gc.getInfoTypeForString('PROMOTION_FATIGUED')) and (pUnit.getUnitCombatType() == gc.getInfoTypeForString('UNITCOMBAT_ANIMAL'))):
 					pUnit.setHasPromotion(gc.getInfoTypeForString('PROMOTION_FATIGUED'), True)
+					if pUnit.getUnitType() == gc.getInfoTypeForString('UNIT_GIANT_SPIDER')):
+						oNewUnit = gc.getInfoTypeForString('UNIT_BABY_SPIDER')
+					else:
+						oNewUnit = pUnit.getUnitType()
 					newUnit = pPlayer.initUnit(oNewUnit, pUnit.getX(), pUnit.getY(), UnitAITypes.UNITAI_ATTACK, DirectionTypes.DIRECTION_SOUTH)
 					self.young(pUnit,newUnit)
 
