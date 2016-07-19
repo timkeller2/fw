716523822caed23141fe965ef4b95f58a38cb22f
timkeller2 <tim@kellpro.com>
2016-07-18 23:49:55 -0500

    Animals have young 1/3% per level after level 5.  Fortifying increases their effective level 1 per fortify level.

--- python/CustomFunctions.py
+++ python/CustomFunctions.py
@@ -3098,7 +3098,7 @@ class CustomFunctions:
 					oNewUnit.convert(pUnit)
 
 				## Animals can have young
-				if (bCanCreateUnit and CyGame().getSorenRandNum(300, "NewAnimal") < pUnit.getLevel() - 1 and not pUnit.isHasPromotion(gc.getInfoTypeForString('PROMOTION_FATIGUED')) and (pUnit.getUnitCombatType() == gc.getInfoTypeForString('UNITCOMBAT_ANIMAL'))):
+				if (bCanCreateUnit and CyGame().getSorenRandNum(300, "NewAnimal") < pUnit.getLevel() - 5 + pUnit.getFortifyTurns() and not pUnit.isHasPromotion(gc.getInfoTypeForString('PROMOTION_FATIGUED')) and (pUnit.getUnitCombatType() == gc.getInfoTypeForString('UNITCOMBAT_ANIMAL'))):
 					pUnit.setHasPromotion(gc.getInfoTypeForString('PROMOTION_FATIGUED'), True)
 					if pUnit.getUnitType() == gc.getInfoTypeForString('UNIT_GIANT_SPIDER'):
 						oNewUnit = gc.getInfoTypeForString('UNIT_BABY_SPIDER')
